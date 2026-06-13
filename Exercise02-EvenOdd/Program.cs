51a3367852020cefc2b5563f91dce9643094fb06
Bahadir Yilmaz <bado246akr@gmail.com>
2026-06-13 16:23:01 +0300

    Exercise 02 - Even Odd Checker

---
+++ Exercise02-EvenOdd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static bool CiftMi(int sayi)
+    {
+        return sayi % 2 == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (CiftMi(sayi))
+        {
+            Console.WriteLine($"{sayi} çift sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{sayi} tek sayıdır.");
+        }
+    }
+}
